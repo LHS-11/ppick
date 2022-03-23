a6d5df796b00630fcf912e7d9421ed575a7154ee
ktykty0722 <ktykty0722@naver.com>
2022-03-24 00:20:51 +0900

    Initial commit

---
+++ src/main/java/com/warrenverr/ppick/PpickApplication.java
@@ -0,0 +1,13 @@
+package com.warrenverr.ppick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warrenverr/ppick/PpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warrenverr.ppick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
