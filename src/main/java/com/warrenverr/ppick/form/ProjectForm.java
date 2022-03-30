bd0beea4f781fa0b1f0e7f8bc20a5ef4688dd827
Lim <ghktjq1119@naver.com>
2022-03-30 20:31:44 +0900

    수정

--- src/main/java/com/warrenverr/ppick/form/ProjectForm.java
+++ src/main/java/com/warrenverr/ppick/form/ProjectForm.java
@@ -7,4 +7,7 @@ import lombok.Setter;
 @Setter
 public class ProjectForm {
 
+
+    public Project()
+
 }
--- src/main/java/com/warrenverr/ppick/model/Project.java
+++ src/main/java/com/warrenverr/ppick/model/Project.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @EntityListeners(AuditingEntityListener.class)
 public class Project {
 
