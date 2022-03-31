20b8f22d1b49c757ed17e099a442196d72363f78
ktykty0722 <ktykty0722@naver.com>
2022-03-31 12:39:21 +0900

    Project Cotroller 추가, ProjectService 수정

--- src/main/java/com/warrenverr/ppick/dto/ProjectDto.java
+++ src/main/java/com/warrenverr/ppick/dto/ProjectDto.java
@@ -1,7 +1,5 @@
 package com.warrenverr.ppick.dto;
 
-import com.warrenverr.ppick.model.Comment;
-import com.warrenverr.ppick.model.User;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
@@ -23,7 +21,7 @@ public class ProjectDto {
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
     private int views;
-    private Set<User> likes;
-    private User author;
-    private List<Comment> commentList;
+    private Set<UserDto> likes;
+    private UserDto author;
+    private List<CommentDto> commentList;
 }
