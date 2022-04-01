3415eefad0e729bf36b5703d4dcd64c3ed7a863b
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2022-04-02 01:15:50 +0900

    kakao RESTAPI Developing..

--- src/main/java/com/warrenverr/ppick/controller/UserController.java
+++ src/main/java/com/warrenverr/ppick/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.warrenverr.ppick.controller;
 
+import com.warrenverr.ppick.Kakao.KakaoAPI;
 import com.warrenverr.ppick.dto.UserDto;
 import com.warrenverr.ppick.form.UserCreateForm;
+import com.warrenverr.ppick.form.UserLoginForm;
 import com.warrenverr.ppick.model.User;
 import com.warrenverr.ppick.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +13,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @Controller
 @SessionAttributes("userInfo")
+@ResponseBody
 public class UserController {
 
     @ModelAttribute("userInfo")
@@ -30,45 +35,66 @@ public class UserController {
         return "signup_form";
     }
 
-    @PostMapping("/singup")
-    public String signup(UserCreateForm userCreateForm, BindingResult bindingResult) {
+    @PostMapping("/signup")
+    public UserDto signup(UserCreateForm userCreateForm, BindingResult bindingResult) {
+        UserDto dto = null;
         if(bindingResult.hasErrors())
-            return "signup_form";
+            return dto;
+
         //TO DO : 다른 예외처리 해주기 (이메일입력안할시 말고 다른 경우)
         if(userCreateForm.getEmail() == "") {
             bindingResult.rejectValue("email", "emaill will be not null", "이메일을 입력해주세요.");
-            return "signup_form";
+            return dto;
         }
         try {
-            userService.signup(userCreateForm);
+            dto = userService.signup(userCreateForm);
         }catch(DataIntegrityViolationException e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자 입니다.");
-            return "signup_form";
+            return dto;
         }catch(Exception e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", e.getMessage());
-            return "signup_form";
+            return dto;
         }
-        return "redirect:/";
+        return dto;
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute("userInfo") String email, Model model) {
-        if(this.userService.loginByEmail(email) == null) {
-            return "signup_form";
+    public UserDto login(HttpServletRequest req, UserLoginForm userLoginForm, Model model) {
+        KakaoAPI kakaoAPI = new KakaoAPI();
+        String code = req.getParameter("code");
+        String access_Token = kakaoAPI.getAccessTocken(code);
+
+        System.out.println("Access_Token : " + access_Token);
+
+        HashMap<String, Object> kakaoInfo = kakaoAPI.getUserInfo(access_Token);
+
+        String email = kakaoInfo.get("email").toString();
+        String sns_id = kakaoInfo.get("sns_id").toString();
+        //카카오 정보 받아온 후 회원가입 탭으로 이동시켜주기
+        if(email == null || sns_id == null) {
+            
         }
+
+
+
+        System.out.println("@@@ : " + email);
         UserDto userDto = this.userService.loginByEmail(email);
+        if(this.userService.loginByEmail(email) == null) {
+            return userDto;
+        }
+
         //위에 어노테이션이랑 동일한 어트리뷰트 네임이면 session에 넘어가는 것 같아요.
         model.addAttribute("userInfo", userDto);
 
-        return "redirect:/";
+        return userDto;
     }
 
     @GetMapping("/info")
-    public String selectInfo(Model model) {
+    public UserDto selectInfo(Model model) {
         model.getAttribute("userInfo");
-
-        return "mypage";
+        UserDto userDto = (UserDto) model.getAttribute("userInfo");
+        return userDto;
     }
 }
